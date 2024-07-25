39b060bf15b54f43bafd1f71cd860b628a2ccf61
Jozka Siregar <jozkasiregar@gmail.com>
2024-07-25 19:13:46 +0700

    reload and next level

--- The Green Rocket/Assets/Scripts/CollisionHandler.cs
+++ The Green Rocket/Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float loadDelay = 2f;
+
     private void OnCollisionEnter(Collision collision)
     {
         switch (collision.gameObject.tag)
@@ -12,16 +16,51 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Hello");
                 break;
             case "Hostile":
-                Debug.Log("Explode");
+                StartCrashSequence();
                 break;
             case "Fuel":
                 Debug.Log("Refuel");
                 break;
             case "Finish":
-                Debug.Log("Next level");
+                StartFinishSequence();
                 break;
             default:
                 break;
+        }
+    }
+
+    private void StartCrashSequence()
+    {
+        // to do add sfx
+        // to do add particle effect
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    private void StartFinishSequence()
+    {
+        // to do add sfx
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", loadDelay);
+    }
+
+    private void NextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        int totalScene = SceneManager.sceneCountInBuildSettings;
+        if (nextSceneIndex == totalScene)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(nextSceneIndex);
         }
     }
 }
